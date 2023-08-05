ae2c221caa44c141ec28f74d1eb1da0cddf552b0
Makarov Sergey <sgmakgg@gmail.com>
2023-08-05 19:56:47 +0300

    get

--- hw4095/server/server4095.js
+++ hw4095/server/server4095.js
@@ -30,13 +30,54 @@ function logLineSync(logFilePath,logLine) {
 
 
 webserver.post('/postman', (req, res) => {
-    logLineSync(logFilePath,`[${port}] `+'"/postman" endpoint called');
+    logLineSync(logFilePath,`[${port}] `+'"/postman" endpoint called ' + `get req.body:  ${JSON.stringify(req.body)}`);
 
+        if(req.body.method === 'GET' && req.body.parameters.length === 0){
+            let fetchConfig={
+                URL: req.body.url,
+                method: req.body.method
+            };
 
-    res.status(200);
+            fetchConfig.headers = ClientHeadersToReqHeaderParser(req.body.contentTypes);
+            logLineSync(logFilePath,`[${port}] `+'"/postman" endpoint ' + `created fetchConfig:  ${JSON.stringify(fetchConfig)}`);
+
+            SendResponseToClient(fetchConfig, res);
+        }
 });
 
 
 webserver.listen(port,()=>{
     logLineSync(logFilePath,"web server3095 running on port "+port);
 });
+
+async function SendResponseToClient  (fetchConfig, res){
+    let clientResponseObj = {};
+    try {
+        let response=await fetch(fetchConfig.URL, fetchConfig);
+
+        if (!response.ok) {
+            throw new Error("fetch error " + response.status);
+        }
+
+        clientResponseObj.status = response.status;
+        clientResponseObj.contentType = response.headers.get('Content-Type');
+        clientResponseObj.headers = {};
+        for (let [key, value] of response.headers) {
+            clientResponseObj.headers[key] = value;
+        }
+
+        let data=await response.json();
+        clientResponseObj.body =  JSON.stringify(data);
+
+        res.send(JSON.stringify(clientResponseObj)).status(200);
+    }
+    catch ( error )  {
+        console.log(error.message);
+    }
+}
+
+function ClientHeadersToReqHeaderParser(reqConTypesObj){
+    let tempObj = {};
+    reqConTypesObj.forEach(item=> tempObj[item.key] = item.value);
+    return tempObj;
+}
