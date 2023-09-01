ade41c51837e9d5be9c2fa05b93291610ed682c4
Makarov Sergey <sgmakgg@gmail.com>
2023-09-01 14:39:50 +0300

    crud imp

--- sqlexplorer/server6195.js
+++ sqlexplorer/server6195.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
-const fs = require('fs');
-const fsp = fs.promises;
 const {logLineAsync, escapeHTML} = require("./utils");
+
+const helmet = require('helmet');
 
 const webserver = express();
 webserver.use(express.urlencoded({extended:true}));
+
 
 const port = 6195;
 const logFilePath = path.join(__dirname, '_server.log');
 
 const mysql=require("mysql");
 const {poolConfig} = require("./db/poolConfig");
-const {newConnectionFactory, selectQueryFactory} = require("./db/db_utils");
+const {newConnectionFactory, selectQueryFactory, modifyQueryFactory} = require("./db/db_utils");
 let pool = mysql.createPool(poolConfig);
 
 function reportServerError(error,res) {
@@ -22,9 +24,15 @@ function reportServerError(error,res) {
 }
 
 function reportRequestError(error,res) {
-    res.status(400).end();
+    res.status(400).send(error);
     logLineAsync(logFilePath,`[${port}] `+error);
 }
+
+webserver.use(helmet({
+    contentSecurityPolicy: false,
+}));
+
+webserver.use(bodyParser.text({ type: 'text/html' }));
 
 webserver.use(function (req, res, next) {
     logLineAsync(logFilePath,`[${port}] `+"static server called, originalUrl="+req.originalUrl);
@@ -50,13 +58,86 @@ webserver.get('/read', async (req, res) => {
         res.send(sqlResult);
     }
     catch ( error ) {
-        reportServerError(error,res); // сюда прилетят любые ошибки
+        reportServerError(error,res);
     }
     finally {
         if ( connection )
-            connection.release(); // соединение надо закрыть (вернуть в пул) независимо от успеха/ошибки
+            connection.release();
     }
 });
+
+//CREATE
+webserver.post('/create', async(req,res)=>{
+    logLineAsync(logFilePath,`[${port}] `+"/create endpoint called");
+
+    let connection=null;
+
+    try {
+        connection=await newConnectionFactory(pool,res);
+
+        let queryText = req.body;
+        let sqlResult = await modifyQueryFactory(connection, queryText, []);
+
+        res.send(sqlResult);
+    }
+    catch ( error ) {
+        console.log(error);
+        reportRequestError(error,res);
+    }
+    finally {
+        if ( connection )
+            connection.release();
+    }
+});
+
+//UPDATE
+webserver.put('/modify', async (req, res) =>{
+    logLineAsync(logFilePath,`[${port}] `+"/modify endpoint called");
+
+    let connection=null;
+
+    try {
+        connection=await newConnectionFactory(pool,res);
+
+        let queryText = req.body;
+        let data = await modifyQueryFactory(connection, queryText, []);
+
+        res.send(data);
+    }
+    catch ( error ) {
+        console.log(error);
+        reportServerError(error,res);
+    }
+    finally {
+        if ( connection )
+            connection.release();
+    }
+});
+
+//delete
+webserver.delete('/delete', async (req, res) =>{
+    logLineAsync(logFilePath,`[${port}] `+"/delete endpoint called");
+
+    let connection=null;
+
+    try {
+        connection=await newConnectionFactory(pool,res);
+
+        let queryText = req.body;
+        let data = await modifyQueryFactory(connection, queryText, []);
+
+        res.send(data);
+    }
+    catch ( error ) {
+        console.log(error);
+        reportServerError(error,res);
+    }
+    finally {
+        if ( connection )
+            connection.release();
+    }
+});
+
 
 webserver.listen(port,()=>{
     logLineAsync(logFilePath,"web server running on port "+port);
