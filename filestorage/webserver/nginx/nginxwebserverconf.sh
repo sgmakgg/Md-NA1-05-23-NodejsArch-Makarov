71393f8d5f60aa36b707ccb975a67e65c5412fd9
Makarov Sergey <sgmakgg@gmail.com>
2023-09-21 14:44:38 +0300

    nginx conf

--- filestorage/webserver/nginx/nginxwebserverconf.sh
+++ filestorage/webserver/nginx/nginxwebserverconf.sh
@@ -1,6 +1,6 @@
 export HTTP_PORT=5695 
 export WS_PORT=56951 
 export DOLLAR='$'
-envsubst < /etc/nginx/conf.d/main.conf.template > /etc/nginx/conf.d/fstorage.conf 
+envsubst < /etc/nginx/conf.d/main.conf.template > /etc/nginx/conf.d/webserver.conf
 nginx -c /etc/nginx/nginx.conf 
 nginx -s reload
