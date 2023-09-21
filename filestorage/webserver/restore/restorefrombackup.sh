0358b3cb90a61d83fd15d9b65c74914ae0a047da
Makarov Sergey <sgmakgg@gmail.com>
2023-09-21 17:41:54 +0300

    restore

---
+++ filestorage/webserver/restore/restorefrombackup.sh
@@ -0,0 +1,5 @@
+mysql --login-path=/home/chubby/.my.conf fstorage_db < /home/chubby/git/Md-NA1-05-23-NodejsArch-Makarov/filestorage/backup/fstorage_db_backup.sql
+cd /home/chubby/git/Md-NA1-05-23-NodejsArch-Makarov/filestorage/backup
+tar -xzf filestorage.tar.gz -C /home/chubby/git/Md-NA1-05-23-NodejsArch-Makarov/filestorage
+
+sh -c 'cd /home/chubby/git/Md-NA1-05-23-NodejsArch-Makarov/filestorage/backup &&  tar -xzf filestorage.tar.gz -C /home/chubby/git/Md-NA1-05-23-NodejsArch-Makarov/filestorage'
